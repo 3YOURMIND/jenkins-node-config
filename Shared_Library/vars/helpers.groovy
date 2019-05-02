df1d9500b6f602afda601522c71ebe2fe2d7d598
Nando <nt@3yourmind.com>
2019-05-02 15:26:49 +0200

    Add Jenkins shared_library for all pipelines

---
+++ Shared_Library/vars/helpers.groovy
@@ -0,0 +1,19 @@
+def getTagName() {
+  gitOut = sh (
+    script: "git --no-pager describe --exact-match || true",
+    returnStdout: true
+  ).trim()
+
+  return gitOut.startsWith("fatal:") ? "" : gitOut
+}
+
+def authenticateWithECR(){
+    sh "eval \$(aws ecr get-login --no-include-email --region eu-central-1)"
+}
+
+def retagAndPushImage(String tag){
+  sh "docker tag ${PROJECT}:${BRANCH_NAME} ${env.ECR_ENDPOINT}/${PROJECT}:${tag}"
+  sh "docker push ${env.ECR_ENDPOINT}/${PROJECT}:${tag}"
+}
+
+return this
