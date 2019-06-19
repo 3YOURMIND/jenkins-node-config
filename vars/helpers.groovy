c0675b7249584bf983c03ae4a008ebd2025c26ee
Nando Theessen <nt@3yourmind.cm>
2019-06-19 13:48:59 +0200

    Re-add tag param to retagandpush function

--- vars/helpers.groovy
+++ vars/helpers.groovy
@@ -20,7 +20,7 @@ def ECRLogin(){
     sh "eval \$(aws ecr get-login --no-include-email --region eu-central-1)"
 }
 
-def retagAndPushImage(String project, String branch_name){
+def retagAndPushImage(String project, String branch_name, String tag){
   sh "docker tag ${project}:${branch_name} ${env.ECR_ENDPOINT}/${project}:${branch_name}"
   sh "docker push ${env.ECR_ENDPOINT}/${project}:${branch_name}"
 }
