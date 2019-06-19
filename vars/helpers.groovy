db61ec3e7b68832e03b3afe2c32653d4814e464a
Nando Theessen <nt@3yourmind.cm>
2019-06-19 13:40:40 +0200

    Remove logic evolving around tags from retag&push

--- vars/helpers.groovy
+++ vars/helpers.groovy
@@ -20,15 +20,9 @@ def ECRLogin(){
     sh "eval \$(aws ecr get-login --no-include-email --region eu-central-1)"
 }
 
-def retagAndPushImage(String project, String branch_name, String tag){
-  if (tag != "") {
-    tag_name = tag.replaceAll('/', '-').stripIndent()
-    sh "docker tag ${project}:${branch_name} ${env.ECR_ENDPOINT}/${project}:${tag_name}"
-    sh "docker push ${env.ECR_ENDPOINT}/${project}:${tag_name}"
-  } else {
-    sh "docker tag ${project}:${branch_name} ${env.ECR_ENDPOINT}/${project}:${branch_name}"
-    sh "docker push ${env.ECR_ENDPOINT}/${project}:${branch_name}"
-  }
+def retagAndPushImage(String project, String branch_name){
+  sh "docker tag ${project}:${branch_name} ${env.ECR_ENDPOINT}/${project}:${branch_name}"
+  sh "docker push ${env.ECR_ENDPOINT}/${project}:${branch_name}"
 }
 
 return this
